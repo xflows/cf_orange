b0cb8de25428352e38eca87a71702f7180bb8fcf
Anze Vavpetic <anze.vavpetic@ijs.si>
2017-10-18 14:48:18 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+The ClowdFlows 2.0 core Package. Contains Orange widgets for ClowdFlows 2.0.
+To be used along with ClowdFlows 2.0.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+]
+
+dist = setup(
+    name='cf_orange',
+    version='0.1',
+    author='Anze Vavpetic',
+    description='Package providing Orange widgets for ClowdFlows 2.0',
+    url='https://github.com/xflows/cf_orange',
+    license='MIT License',
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Orange==2.5a4',
+    ],
+)
